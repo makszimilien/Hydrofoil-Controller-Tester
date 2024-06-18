94186e03c32322c5484a188691fe58001eba959a
makszimilien <nagy.tamas.kvk@gmail.com>
2024-06-18 20:46:55 +0200

    Add inputs for run_measurement_motion_sequence, fix run_measurement, use DTR and RTS

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import serial
-from datetime import datetime
 import matplotlib.pyplot as plt
-import numpy as np
-import array as arr
 from printer import Printer
 import sys
 import threading
-import time
 
 test_rig = Printer()
 is_finished = False
@@ -14,11 +10,12 @@ measurements = []
 
 
 # Test-rig motion control
-def run_measurement_motion_sequence(min_speed=15000, max_speed=16000, step=1000):
+def run_measurement_motion_sequence():
+    global min_speed, max_speed, step
     test_rig.send_gcode("G28 X0")
     test_rig.send_gcode("G90")
     for speed in range(min_speed, max_speed + 1, step):
-        test_rig.send_gcode(f"G1 X160 F{speed}")
+        test_rig.send_gcode(f"G1 X145 F{speed}")
         test_rig.send_gcode(f"G1 X0 F{speed}")
     test_rig.wait_move()
     global is_finished
@@ -31,6 +28,8 @@ def init_serial(baud):
         s = serial.Serial("/dev/ttyUSB0", baud, timeout=1)
         # If the above line doesn't raise an exception, the port is successfully opened
         print("Serial port /dev/ttyUSB0 is open.")
+        s.dtr = False
+        s.rts = False
         return s
     except serial.SerialException:
         print("Failed to open /dev/ttyUSB0. Trying /dev/ttyUSB1 instead.")
@@ -46,11 +45,14 @@ def init_serial(baud):
 def run_measurement():
     while not is_finished:
         global serial
-        buffer = serial.readline().decode('utf-8')
-        items = buffer.split(':')
+        try:
+            buffer = serial.readline().decode("utf-8")
+        except:
+            continue
+        items = buffer.split(":")
         measurement = {}
 
-        if items[0] == "input":
+        if items[0] == "input" and len(items) == 18:
             for i in range(0, len(items), 2):
                 key = items[i]
                 value = items[i + 1]
@@ -61,41 +63,37 @@ def run_measurement():
                     pass
 
                 measurement[key] = value
-        if 'input' in measurement:
+
+        if "input" in measurement:
             global measurements
             measurements.append(measurement)
-    # return measurements
 
 
 def plot_graph(data):
     inputs = [entry["input"] for entry in data]
-    setpoints = [entry['setpoint'] for entry in data]
-    outputs = [entry['output'] for entry in data]
-    pwms = [entry['pwm'] for entry in data]
-    measured = [entry['measured'] for entry in data]
-    kps = [entry['kp'] for entry in data]
-    kis = [entry['ki'] for entry in data]
-    kds = [entry['kd'] for entry in data]
-    distances = [entry['distance'] for entry in data]
+    setpoints = [entry["setpoint"] for entry in data]
+    outputs = [entry["output"] for entry in data]
+    pwms = [entry["pwm"] for entry in data]
+    measured = [entry["measured"] / 3.0 for entry in data]
+    kps = [entry["kp"] for entry in data]
+    kis = [entry["ki"] for entry in data]
+    kds = [entry["kd"] for entry in data]
+    distances = [entry["distance"] * -12 for entry in data]
 
     # Create a figure and axis for the plot
     fig, ax = plt.subplots()
 
     # Plot the data
-    ax.plot(inputs, label='Input')
-    ax.plot(setpoints, label='Setpoint')
-    ax.plot(outputs, label='Output')
-    ax.plot(pwms, label='PWM')
-    ax.plot(measured, label='Measured')
-    ax.plot(kps, label='KP')
-    ax.plot(kis, label='KI')
-    ax.plot(kds, label='KD')
-    ax.plot(distances, label='Distance')
+    ax.plot(inputs, label="Input")
+    ax.plot(outputs, label="Output")
+    ax.plot(pwms, label="PWM Input")
+    ax.plot(measured, label="Measured")
+    ax.plot(distances, label="Distance")
 
     # Set plot title and labels
-    ax.set_title('Measurements')
-    ax.set_xlabel('Index')
-    ax.set_ylabel('Value')
+    ax.set_title("Measurements")
+    ax.set_xlabel(f"Index, Setpoint: {setpoints[0]}, Kp: {kps[0]}, Ki: {kis[0]}, Kd: {kds[0]}")
+    ax.set_ylabel("Value")
 
     # Add a legend
     ax.legend()
@@ -106,9 +104,13 @@ def plot_graph(data):
 
 serial = init_serial(19200)
 
-# minimum_speed = int(input("Min speed: "))
-# maximum_speed = int(input("Max speed: "))
-# step_size = int(input("Step: "))
+# min_speed = int(input("Min speed: "))
+# max_speed = int(input("Max speed: "))
+# step = int(input("Step: "))
+
+min_speed = 5000
+max_speed = 5001
+step = 1
 
 # Create threads
 thread1 = threading.Thread(target=run_measurement_motion_sequence)
